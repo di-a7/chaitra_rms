560facd55f94e7d3ae1ea02e64bb0feddafb1fc6
diya <shresthadiya59@gmail.com>
2025-04-23 15:56:15 +0545

    model creation complete

--- rms/models.py
+++ rms/models.py
@@ -1,6 +1,8 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model 
 # Create your models here.
+
+User = get_user_model()
 class Category(models.Model):
    name = models.CharField(max_length=100)
    
@@ -14,3 +16,38 @@ class Food(models.Model):
    
    def __str__(self):
       return self.name
+
+class Table(models.Model):
+   number = models.IntegerField()
+   available = models.BooleanField(default = True)
+   
+   def __str__(self):
+      return f"Table no.{self.number}- {self.available}"
+
+class Order(models.Model):
+   PENDING = 'P'
+   COMPLETED = 'C'
+   STATUS_CHOICE = {
+      PENDING :"Pending",
+      COMPLETED :"Completed"
+   }
+   
+   PAID = 'P'
+   UNPAID = 'U'
+   
+   PAYMENT_CHOICE = {
+      PAID : "Paid",
+      UNPAID : "Not Paid"
+   }
+   
+   user = models.ForeignKey(User, on_delete=models.CASCADE)
+   total_price = models.IntegerField()
+   status = models.CharField(max_length=1, choices=STATUS_CHOICE, default=PENDING)
+   payment_status = models.CharField(max_length=1, choices=PAYMENT_CHOICE, default=UNPAID)
+   
+   def __str__(self):
+      return f"{self.user} - {self.status}"
+
+class OrderItem(models.Model):
+   food = models.ForeignKey(Food, on_delete=models.PROTECT)
+   order = models.ForeignKey(Order, on_delete=models.PROTECT)
